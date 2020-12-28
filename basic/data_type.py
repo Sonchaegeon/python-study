7b2105b46729de4661e7a863d9d9d5722c416335
Chaegeon <thscorjs1002@naver.com>
2020-12-28 23:05:42 +0900

    [ADD] data_type.py

---
+++ basic/data_type.py
@@ -0,0 +1,23 @@
+# int는 숫자
+price = 80000
+print(price)
+print(type(price))
+
+# float 소수점을 가진 숫자
+circle = 3.14
+print(circle)
+print(type(circle))
+
+# string은 문자열
+name = "손채건"
+print(name)
+print(type(name))
+
+# boolean -> true or false
+first_bool = True
+two_bool = False
+
+print(first_bool)
+print(type(first_bool))
+print(two_bool)
+print(type(two_bool))
