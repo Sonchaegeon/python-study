85c7238987d89ac1992ecda0db6731d9c02d8da3
Chaegeon <thscorjs1002@naver.com>
2020-12-29 10:16:13 +0900

    [ADD] calculations.py

---
+++ basic/calculations.py
@@ -0,0 +1,26 @@
+# int 더하기
+price = 2000
+price2 = 4000
+
+plus = price + price2
+print(plus)
+
+# int 빼기
+minus = price - price2
+print(minus)
+minus = price2 - price
+print(minus)
+
+# int 곱하기
+multi = price * price2
+print(multi)
+
+# int 나누기
+divi = price2 / price
+divi2 = price2 // price
+print(divi)
+print(divi2)
+
+# int 나머지
+share = price2 % price
+print(share)
